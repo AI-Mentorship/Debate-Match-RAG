b2d5ba10d20d6f0e2cea94b51b6cd6692d639a09
Khang Doan <kdoan.personal@gmail.com>
2025-10-15 21:22:47 -0500

    Added basic frontend code

--- src/main.py
+++ src/main.py
@@ -33,6 +33,10 @@ def setup_database():
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     setup_database()
     app.run(debug=False)
