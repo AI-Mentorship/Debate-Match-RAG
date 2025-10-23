7b07d6cf63b6128203c6a54fb644b961a20e9836
Khang Doan <kdoan.personal@gmail.com>
2025-10-23 16:24:21 -0500

    Added background image and global styles

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -10,7 +10,36 @@ function App() {
 
   return (
     <div className="min-h-screen font-noto-sans overflow-hidden">
+      {/* Background */}
+      <div 
+        className="fixed inset-0 z-0"
+        style={{
+          backgroundImage: 'url("/src/assets/img/background.png")',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundAttachment: 'fixed'
+        }}
+      >
+      </div>
       
+      {/* Content */}
+      <div className="relative z-10 min-h-screen flex flex-col">
+        
+      </div>
+
+      {/* Hide scrollbar */}
+      <style jsx global>{`
+        body {
+          overflow: hidden;
+        }
+
+        ::-webkit-scrollbar {
+          display: none;
+        }
+          
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+      `}</style>
     </div>
   )
 }
