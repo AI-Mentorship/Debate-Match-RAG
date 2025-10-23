1cbb4ae8749f3255b8668790abf6ec2a79ef99f3
Khang Doan <kdoan.personal@gmail.com>
2025-10-23 16:26:28 -0500

    Created navigation bar with logo

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -24,7 +24,26 @@ function App() {
       
       {/* Content */}
       <div className="relative z-10 min-h-screen flex flex-col">
-        
+        {/* Navigation Bar */}
+        <nav className="px-8 py-6 flex items-center justify-between">
+          {/* Logo - Top Left */}
+          <div className="flex items-center space-x-3 flex-1">
+            <h1 className="text-xl font-bold">
+              <span className="text-electric-purple">DebateMatch</span>
+              <span className="text-white">.RAG</span>
+            </h1>
+          </div>
+
+          {/* Navigation Links - Truly Centered */}
+          <div className="hidden md:flex flex-1 justify-center">
+            
+          </div>
+
+          {/* Get Started Button - Top Right */}
+          <div className="flex-1 flex justify-end">
+            
+          </div>
+        </nav>
       </div>
 
       {/* Hide scrollbar */}
@@ -36,7 +55,7 @@ function App() {
         ::-webkit-scrollbar {
           display: none;
         }
-          
+
         -ms-overflow-style: none;
         scrollbar-width: none;
       `}</style>
