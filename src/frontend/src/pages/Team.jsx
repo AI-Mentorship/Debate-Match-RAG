0c31b6342c13e6b1ef15c9f49f050804a3b78bd9
Khang Doan <kdoan.personal@gmail.com>
2025-11-06 14:34:40 -0600

    Added instagram link buttons

--- src/frontend/src/pages/Team.jsx
+++ src/frontend/src/pages/Team.jsx
@@ -39,6 +39,7 @@ function Team() {
       role: "Project Lead",
       description: "B.S. in Computer Science",
       linkedin: "https://www.linkedin.com/in/adyadhanasekar/",
+      instagram: "",
       image: "src/assets/img/Adya_Dhanasekar.png"
     },
     {
@@ -47,6 +48,7 @@ function Team() {
       role: "UX/UI Designer",
       description: "B.S. in Computer Science",
       linkedin: "https://www.linkedin.com/in/raisa-reza/",
+      instagram: "",
       image: ""
     },
     {
@@ -55,6 +57,7 @@ function Team() {
       role: "Back-End Developer",
       description: "B.S. in Computer Science",
       linkedin: "https://www.linkedin.com/in/yakina-azza/",
+      instagram: "",
       image: ""
     },
     {
@@ -63,6 +66,7 @@ function Team() {
       role: "QA Pipeline Lead",
       description: "B.S. in Computer Science",
       linkedin: "https://www.linkedin.com/in/sadwitha1161/",
+      instagram: "",
       image: ""
     },
     {
@@ -71,6 +75,7 @@ function Team() {
       role: "Project Lead",
       description: "B.S. in Computer Science",
       linkedin: "https://www.linkedin.com/in/shivam-singh-9935ab305/",
+      instagram: "",
       image: ""
     },
     {
@@ -79,6 +84,7 @@ function Team() {
       role: "Full Stack Developer",
       description: "B.S. in Computer Science with focus on AI/ML",
       linkedin: "https://www.linkedin.com/in/khangdoan514/",
+      instagram: "https://www.instagram.com/itsmekhangdoan/",
       image: "src/assets/img/Sample_PFP.jpeg"
     },
     {
@@ -87,6 +93,7 @@ function Team() {
       role: "Full Stack Developer",
       description: "B.S. in Computer Science",
       linkedin: "https://www.linkedin.com/in/pavan-arani-15954426a/",
+      instagram: "",
       image: "src/assets/img/Pavan_Arani.png"
     },
     {
@@ -95,6 +102,7 @@ function Team() {
       role: "Full Stack Developer",
       description: "M.S. in Computer Science",
       linkedin: "https://www.linkedin.com/in/satyank-nadimpalli/",
+      instagram: "",
       image: "src/assets/img/Satyank_Nadimpalli.jpg"
     }
   ]
@@ -175,13 +183,14 @@ function Team() {
                   {member.description}
                 </p>
 
-                {/* LinkedIn Button */}
-                <div className="text-center relative z-10">
+                {/* Social Media */}
+                <div className="text-center relative z-10 flex justify-center space-x-2">
+                  {/* LinkedIn */}
                   <a
                     href={member.linkedin}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className={`inline-flex items-center justify-center w-8 h-8 rounded-full transition-all duration-500 transform group-hover:scale-110 group-hover:rotate-12 ${member.bgColor} border border-white/20 hover:border-white/40`}
+                    className={`inline-flex items-center justify-center w-8 h-8 rounded-full transition-all duration-500 transform group-hover:scale-110 group-hover:rotate-12 bg-gradient-to-r ${member.color} border border-white/20 hover:border-white/40`}
                   >
                     <svg 
                       className="w-4 h-4 text-white" 
@@ -189,6 +198,22 @@ function Team() {
                       viewBox="0 0 24 24"
                     >
                       <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                    </svg>
+                  </a>
+                  
+                  {/* Instagram */}
+                  <a
+                    href={member.instagram}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={`inline-flex items-center justify-center w-8 h-8 rounded-full transition-all duration-500 transform group-hover:scale-110 group-hover:-rotate-12 bg-gradient-to-r ${member.color} border border-white/20 hover:border-white/40`}
+                  >
+                    <svg 
+                      className="w-4 h-4 text-white" 
+                      fill="currentColor" 
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
                     </svg>
                   </a>
                 </div>
